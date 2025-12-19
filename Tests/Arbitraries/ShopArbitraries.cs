72d298367f58b0aff51810d301900f9891dde780
Iehohapata <148205901+Iehohapata@users.noreply.github.com>
2025-12-19 15:47:18 +0200

    Implemented generators for domain

--- Tests/Arbitraries/ShopArbitraries.cs
+++ Tests/Arbitraries/ShopArbitraries.cs
@@ -1,44 +1,58 @@
 ﻿using FsCheck;
 using FsCheck.Fluent;
 using Shopping_Cart_System.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shopping_Cart_System.Tests.Arbitraries
 {
     public static class ShopArbitraries
     {
+        private const string alphaChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
         public static Arbitrary<Product> Product()
         {
-            throw new NotImplementedException();
+            var gen =
+                from id in NonEmptyAlphaString()
+                from name in NonEmptyAlphaString()
+                from price in Gen.Choose(1, int.MaxValue).Select(p => (decimal)p / 100m)
+                select new Product(id, name, price);
+
+            return Arb.From(gen);
         }
 
         public static Arbitrary<int> Quantity()
         {
-            throw new NotImplementedException();
-
+            return Arb.From(Gen.Choose(1, int.MaxValue));
         }
 
         public static Arbitrary<ShoppingCart> Cart()
         {
-            throw new NotImplementedException();
+            var gen =
+                from products in Product().Generator.ListOf()
+                from quantities in Quantity().Generator.ListOf(products.Count)
+                select BuildCart(products, quantities);
 
-
+            return Arb.From(gen);
         }
 
         private static ShoppingCart BuildCart(List<Product> products, List<int> quantities)
         {
-            throw new NotImplementedException();
+            var cart = new ShoppingCart();
 
+            foreach (var pair in products
+                .Zip(quantities, (p, q) => (p, q))
+                .GroupBy(x => x.p.Id)
+                .Select(g => g.First()))
+            {
+                cart.Add(pair.p, pair.q);
+            }
 
+            return cart;
         }
 
         private static Gen<string> NonEmptyAlphaString()
         {
-            throw new NotImplementedException();
+            return Gen.NonEmptyListOf(Gen.Elements(alphaChars.ToCharArray()))
+                    .Select(chars => new string(chars.ToArray()));
         }
     }
 
